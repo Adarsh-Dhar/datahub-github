3f3f977c76cc7615343c8f3e3067099974f2c925
Adarsh Dhar <dharadarsh0@gmail.com>
2026-07-25 18:10:39 +0530

    test: change join condition literal in fct_revenue

--- pr-guardian-demo/models/marts/fct_revenue.sql
+++ pr-guardian-demo/models/marts/fct_revenue.sql
@@ -6,5 +6,4 @@ select
     o.created_at,
     date_trunc('day', o.created_at) as order_date
 from {{ ref('stg_orders') }} o
-join {{ ref('dim_customers') }} c on o.customer_id = c.customer_id and o.order_status = 'completed'
-
+join {{ ref('dim_customers') }} c on o.customer_id = c.customer_id and o.order_status = 'shipped'
