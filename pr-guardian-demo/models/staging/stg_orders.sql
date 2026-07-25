52914b1e8c640a965198aa69c800c9d54826bfb7
Adarsh Dhar <dharadarsh0@gmail.com>
2026-07-25 11:49:25 +0530

    test: add order_day column (additive change)

--- pr-guardian-demo/models/staging/stg_orders.sql
+++ pr-guardian-demo/models/staging/stg_orders.sql
@@ -13,5 +13,6 @@ select
     customer_id,
     order_status,
     cast(order_total as decimal(12, 2)) as order_total,
-    created_at
+    created_at,
+    date_trunc('day', created_at) as order_day
 from source_orders
